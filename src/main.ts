3988bba5c48e48e3b9b80f3b315e48178c32bfc1
Mateus Barcelos <mateuspit@hotmail.com>
2023-09-04 16:29:48 -0300

    fix: deploy

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,7 @@ async function bootstrap() {
     const config = new DocumentBuilder()
         .setTitle("Secure pass - password managment")
         .setDescription(`Navigating the internet can be both enjoyable and perilous. Studies, both national and international, reveal a relentless rise in online scams. This raises the question: how can we protect ourselves? There are various methods, starting with the use of distinct and secure passwords. A secure password should include a mix of characters and numbers and, the longer it is, the better. However, how can we remember lengthy, semantically meaningless passwords? This is where password managers come in! With them, you create just one "master" password, keeping all your other passwords securely stored. So, when needed, you only have to recall the "master" password (which, of course, should also be secure).`)
-        .setVersion("Beta")
-        .addTag("endpoints")
+        .setVersion("Alfa")
         .build();
 
     const document = SwaggerModule.createDocument(app, config);
--- src/wifi/wifi.controller.ts
+++ src/wifi/wifi.controller.ts
@@ -1,7 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { WifiService } from './wifi.service';
-import { ApiOperation } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags("Wifi routes")
 @Controller('wifi')
 export class WifiController {
     constructor(private readonly wifiService: WifiService) { }
